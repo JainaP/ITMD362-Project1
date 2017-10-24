c72d7bce04f2e67fbd88601cb5c1789d9f659f43
JainaP <jparekh1@hawk.iit.edu>
2017-10-24 09:38:14 -0500

    added .js as seperate file

---
+++ script.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+    document.getElementById("myForm").onsubmit = function() {submitpopup()}
+    function submitpopup(){
+	  alert("The form has been submitted");
+	}
+	
+	function confirmEmail() {
+	  var email1 = document.getElementById("email_field").value
+	  var email2 = document.getElementById("email_confirm").value
+	    if(email1 != email2)
+	      {
+		    alert('Email Not Matching');
+		  }  
+	}
+</script>
